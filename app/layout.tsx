fd726e472c5ca0d5f0443b732a079a68e98c9da4
Madhusudan <madhusudanchauhan62@gmail.com>
2025-03-16 22:52:29 +0530

    setup done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans} from "next/font/google";
 import "./globals.css";
+import {cn} from '@/lib/utils'
+
 
 const fontSans = Plus_Jakarta_Sans({
   variable: "--font-sans",
   subsets: ["latin"],
+  weight : ['300', '400', '500', '600', '700']
 });
 
 const fontMono = Plus_Jakarta_Sans({
@@ -25,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${fontSans.variable} ${fontMono.variable} antialiased`}
+        className={ cn('min-h-screen bg-dark-300' , fontSans.variable , fontMono.variable , "antialiased" )}
       >
         {children}
       </body>
